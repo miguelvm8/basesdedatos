efc239b14bce39825012a37663aeb56ef3781dfc
esc_codigo_11 <esc_codigo11@test.com>
2022-09-23 12:06:40 -0500

    agregue ejerciciossql

---
+++ ejercicio1sql.sql
@@ -0,0 +1,13 @@
+-- SHOW databases;
+CREATE DATABASE dept_emp;
+-- USE detp_emp;
+-- DROP DATABASE detp_emp;
+USE dept_emp;
+CREATE TABLE dept(
+deptno NUMERIC(2) PRIMARY KEY,
+dname VARCHAR(12) UNIQUE,
+loc VARCHAR(11) NOT NULL);
+
+-- DESCRIBE dept;
+INSERT INTO dept(deptno, dname,loc) 
+VALUES(10,'ACCOUNTING','NEW_YORK');
